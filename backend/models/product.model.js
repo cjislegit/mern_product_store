0a9f3695ac4d2d3d06d4310357c38bbb2ca6d458
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-10-09 18:04:39 -0700

    Added model for products

---
+++ backend/models/product.model.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
+app.post('/products', async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Please provide all fields' });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error('Error in Create product:', error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
 });
 
 app.listen(8080, () => {
+  connectDB();
   console.log('Server started at http://localhost:8080');
 });
