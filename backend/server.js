286d08a026761266a78b9751468178a3a5238866
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-10-12 19:42:06 -0700

    Created files for routes, controllers, and models

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
+
+import productRoutes from './routes/product.route.js';
+
+dotenv.config();
 
 const app = express();
 
-app.post('/products', async (req, res) => {
-  const product = req.body; // user will send this data
+app.use(express.json()); //allows us ot accept JSON data in the req.body
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'Please provide all fields' });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error('Error in Create product:', error.message);
-    res.status(500).json({ success: false, message: 'Server Error' });
-  }
-});
+app.use('/api/products', productRoutes);
 
 app.listen(8080, () => {
   connectDB();
