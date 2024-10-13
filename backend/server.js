baee1c040d1e9e86d799be2d0f2ee789a715eba7
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-10-12 19:56:18 -0700

    Made a port var and added some error handaling to delete route

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json()); //allows us ot accept JSON data in the req.body
 
 app.use('/api/products', productRoutes);
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log('Server started at http://localhost:8080');
+  console.log('Server started at http://localhost:' + PORT);
 });
