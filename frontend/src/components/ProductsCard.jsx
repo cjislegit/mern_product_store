97913514dd228121936d2baec4d70b9951d64297
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-10-15 11:30:02 -0700

    products now populate on the home page

--- frontend/src/components/ProductsCard.jsx
+++ frontend/src/components/ProductsCard.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Box, Image } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Image,
+  Text,
+  HStack,
+  useColorModeValue,
+  IconButton,
+} from '@chakra-ui/react';
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
 
 const ProductsCard = ({ product }) => {
+  const textColor = useColorModeValue('gray.600', 'gray.200');
+  const bg = useColorModeValue('white', 'GrAnalytics.800');
   return (
     <Box
       shadow='lg'
@@ -18,6 +29,22 @@ const ProductsCard = ({ product }) => {
         w='full'
         objectFit='cover'
       />
+      <Box p={4}>
+        <Heading as='h3' size='md' mb={2}>
+          {product.name}
+        </Heading>
+        <Text fontWeight='bold' fontSize='xl' color={textColor} mb={4}>
+          ${product.price}
+        </Text>
+        <HStack spacing={2}>
+          <IconButton icon={<EditIcon />} colorScheme='blue' />
+          <IconButton
+            icon={<DeleteIcon />}
+            onClick={() => handleDeleteProduct(product._id)}
+            colorScheme='red'
+          />
+        </HStack>
+      </Box>
     </Box>
   );
 };
