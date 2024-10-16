193c919620b2bfe4bf47ab33dd5c52b1fbffbe44
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-10-16 08:19:41 -0700

    Added scripts for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
@@ -9,9 +10,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); //allows us ot accept JSON data in the req.body
 
 app.use('/api/products', productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
   connectDB();
