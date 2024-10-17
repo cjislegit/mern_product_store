2c8dadf56cc94c863629d2fb70efb367268e5625
Carlos J. Ramirez <carlosjramirez11@gmail.com>
2024-10-17 15:56:26 -0700

    Started working on cart page

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,7 +1,74 @@
-import React from 'react';
+import {
+  Container,
+  Heading,
+  useColorModeValue,
+  VStack,
+  Box,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from '@chakra-ui/react';
+import { MdDelete } from 'react-icons/md';
 
 function CartPage() {
-  return <div>CartPage</div>;
+  return (
+    <Container maxW={'container.sm'}>
+      <VStack spacing={8}>
+        <Heading as={'h1'} size={'2x1'} textAlign={'center'} mb={8}>
+          Smart Cart
+        </Heading>
+        {/* <Box
+          w={'full'}
+          bg={useColorModeValue('white', 'gray.800')}
+          p={6}
+          rounded={'lg'}
+          shadow={'md'}
+        ></Box> */}
+        <TableContainer bg={useColorModeValue('white', 'gray.800')}>
+          <Table variant={'simple'}>
+            <Thead>
+              <Tr>
+                <Th>Product</Th>
+                <Th>Price</Th>
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Smart Watch</Td>
+                <Td>$500</Td>
+                <Td>
+                  <MdDelete />
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Smart Watch</Td>
+                <Td>$500</Td>
+                <Td>
+                  <MdDelete />
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Smart Watch</Td>
+                <Td>$500</Td>
+                <Td>
+                  <MdDelete />
+                </Td>
+              </Tr>
+              <Tr>
+                <Td>Total:</Td>
+                <Td>$1500</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </VStack>
+    </Container>
+  );
 }
 
 export default CartPage;
